ae81fbf270a19a86d3f1c231a69d180220bc8811
adam <me@adamperkowski.dev>
2025-10-25 21:48:49 +0200

    feat: plugins yay

--- nix/default.nix
+++ nix/default.nix
@@ -10,4 +10,31 @@ wrapNeovim {
   pname = "anvim";
   versionSuffix = anvimVersion;
   userConfig = ../config;
+
+  plugins = with pkgs.vimPlugins; [
+    catppuccin-nvim
+
+    nvim-lspconfig
+
+    nvim-cmp
+    cmp-nvim-lsp
+    cmp-buffer
+    cmp-path
+
+    indent-blankline-nvim
+    nvim-autopairs
+    undotree
+    gitsigns-nvim
+    copilot-lua
+    nvim-colorizer-lua
+
+    cord-nvim
+    vim-wakatime
+
+    jule-nvim
+  ];
+
+  extraPackages = with pkgs; [
+    nodejs
+  ];
 }
--- nix/wrapper.nix
+++ nix/wrapper.nix
@@ -16,24 +16,45 @@ lib.extendMkDerivation {
     {
       pname ? "neovim",
       versionSuffix ? "wrapped",
+
+      plugins ? [ ],
+
       userConfig,
+
+      extraPackages ? [ ],
     }:
     let
       inherit (neovim-unwrapped) lua;
       inherit (lua.pkgs) luaLib;
 
+      transitiveClosure =
+        p: [ p ] ++ (lib.unique (lib.concatLists (map transitiveClosure p.dependencies or [ ])));
+      findDeps = p: lib.concatMap transitiveClosure p;
+      mkPluginPaths =
+        p: "pack/${pname}/start/${if lib.typeOf p == "path" then baseNameOf p else (p.pname or p.name)}";
+
+      pluginDeps = lib.unique (findDeps plugins ++ plugins);
+
       config = stdenvNoCC.mkDerivation {
         name = "${pname}-config";
         __structuredAttrs = true;
 
+        plugins = pluginDeps;
+        pluginPaths = map mkPluginPaths pluginDeps;
+
+        nativeBuildInputs = [ lndir ];
+
         buildCommand = ''
           mkdir -p $out/lua/${pname}
-          cp -R ${userConfig}/* $out/lua/${pname}
+          lndir "${userConfig}" $out/lua/${pname}
 
           cat > $out/init.lua << EOF
           package.path = "${luaLib.genLuaPathAbsStr lua};$LUA_PATH" .. package.path
           package.cpath = "${luaLib.genLuaCPathAbsStr lua};$LUA_CPATH" .. package.cpath
 
+          vim.env.PATH = vim.env.PATH .. ":${lib.makeBinPath extraPackages}"
+
+          vim.opt.packpath:prepend("$out")
           vim.opt.runtimepath:prepend("$out")
 
           do
@@ -43,6 +64,19 @@ lib.extendMkDerivation {
             end
           end
           EOF
+
+          for (( i = 0; i < "''${#plugins[@]}" ; i++ )); do
+            source="''${plugins[$i]}"
+            dest="$out/''${pluginPaths[$i]}"
+
+            if [ -d "$dest" ]; then
+              echo "$dest already exists, skipping"
+              continue
+            fi
+
+            mkdir -p "$dest"
+            lndir -silent "$source" "$dest"
+          done
         '';
       };
     in
