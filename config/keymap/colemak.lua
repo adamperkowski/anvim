1b456a42be28bccc7b4cb2cf3ba0eb4fb21541c0
koi <me@koi.rip>
2026-01-11 13:14:25 +0100

    colemak: add window keybinds

--- config/keymap/colemak.lua
+++ config/keymap/colemak.lua
@@ -1,8 +1,20 @@
+local mappings = {
+  h = "n",
+  j = "e",
+  k = "i",
+  l = "o",
+}
+
 return function(opts)
-  vim.keymap.set("n", "n", "h", opts)
-  vim.keymap.set("n", "e", "j", opts)
-  vim.keymap.set("n", "i", "k", opts)
-  vim.keymap.set("n", "o", "l", opts)
+  for lhs, rhs in pairs(mappings) do
+    vim.keymap.set("n", lhs, rhs, opts)
+    vim.keymap.set("n", "<C-w>" .. lhs, "<C-w>" .. rhs, opts)
+    vim.keymap.set("n", "<C-w>" .. lhs:upper(), "<C-w>" .. rhs:upper(), opts)
+  end
+
+  vim.keymap.set("n", "<C-w>n", "<C-w>k")
+  vim.keymap.set("n", "<C-w>o", "<C-w>m")
+  vim.keymap.set("n", "<C-w>i", "<nop>")
 
   vim.keymap.set("n", "t", "i", opts)
   vim.keymap.set("n", "k", "o", opts)
